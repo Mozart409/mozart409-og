b965ae0e86a94b2f5bf8e87b35a77b8856a1802d
Amadeus <38767929+Mozart409@users.noreply.github.com>
2020-08-31 10:28:17 +0200

    optimzed logo via svgomg

--- web/index.ts
+++ web/index.ts
@@ -118,7 +118,8 @@ const imageLightOptions: DropdownOption[] = [
   { text: 'Vercel', value: 'https://assets.vercel.com/image/upload/front/assets/design/vercel-triangle-black.svg' },
   { text: 'Next.js', value: 'https://assets.vercel.com/image/upload/front/assets/design/nextjs-black-logo.svg' },
   { text: 'Hyper', value: 'https://assets.vercel.com/image/upload/front/assets/design/hyper-color-logo.svg' },
-  { text: 'Amadeus', value: 'https://og.mozart409.space/logos/amadeus.svg' }
+  { text: 'Amadeus', value: 'https://og.mozart409.space/logos/amadeus.svg' },
+  { text: 'Logo', value: 'https://og.mozart409.space/logos/logo.svg' }
 ]
 
 const imageDarkOptions: DropdownOption[] = [
